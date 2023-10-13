04465c1ec1d0fc05e1aa18e63c730c0da570512a
Uria <dorman010@gmail.com>
2023-10-13 19:49:37 +0100

    getting date-fns to work with everything, import css as well

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var __webpack_exports__ = {};
 /*!***********************!*\
   !*** ./src/script.js ***!
   \***********************/
-console.log('I get called from src')
+
 /******/ })()
 ;
 //# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLG9DIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdG9kb2xpc3QvLi9zcmMvc2NyaXB0LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKCdJIGdldCBjYWxsZWQgZnJvbSBzcmMnKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
