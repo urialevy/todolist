d44474a763c00e78be07970661e3201f861e3456
Uria <dorman010@gmail.com>
2023-10-13 22:59:05 +0100

    separate script and HTML creation functions

--- src/script.js
+++ src/script.js
@@ -1,7 +1,10 @@
 import css from "./style.css"
 import frame from "./frame"
-import appendNode from "./appendNodeToBody"
+import  appendNodeToBody from "./appendNodeToBody"
+import { Task } from "./taskFactory"
+import { compareAsc, format } from 'date-fns'
 
 
+const testTask = new Task('test', 'create a test task', '31.12.2023', 'low')
 
-appendNode(frame())
+appendNodeToBody(frame())
