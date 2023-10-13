138addb5864efbbac980bb66bc887c5c691eb762
Uria <dorman010@gmail.com>
2023-10-13 22:36:10 +0100

    refactor framework creation to include title

--- src/frame.js
+++ src/frame.js
@@ -1,6 +1,6 @@
 export default function frame() {
     const divFrame = document.createElement('div')
     divFrame.id = "container"
-    divFrame.innerHTML = `<div id="header"></div><div id="main"></div><div id=footer></div>`;
+    divFrame.innerHTML = `<div id="header"><h1>To-do list</h1></div><div id="main"></div><div id=footer></div>`;
     return divFrame
 }
