d71b7c4b3cc002d6d927f682102f444404049702
Uria <dorman010@gmail.com>
2023-10-13 22:07:30 +0100

    webpack file creation

--- main.js
+++
@@ -1,9 +0,0 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!***********************!*\
-  !*** ./src/script.js ***!
-  \***********************/
-
-/******/ })()
-;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLG9DIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdG9kb2xpc3QvLi9zcmMvc2NyaXB0LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKCdJIGdldCBjYWxsZWQgZnJvbSBzcmMnKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
