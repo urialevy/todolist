6a9c65b78c56d124c23aeadffb34bd6ec601fcfc
Uria <dorman010@gmail.com>
2023-10-13 20:33:26 +0100

    remove hard code that was used for initial setup

--- src/frame.js
+++ src/frame.js
@@ -1,6 +1,6 @@
 export default function frame() {
     const divFrame = document.createElement('div')
     divFrame.id = "container"
-    divFrame.innerHTML = `<div id="header">HEAD</div><div id="main">MAIN</div><div id=footer>FOOT</div>`;
+    divFrame.innerHTML = `<div id="header"></div><div id="main"></div><div id=footer></div>`;
     return divFrame
 }
