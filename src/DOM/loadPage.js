50f95ffe5aa9fa2bcd823bbc7ff5290040d0efdf
Uria <dorman010@gmail.com>
2023-10-14 16:43:44 +0100

    adding more HTML elements

--- src/DOM/loadPage.js
+++ src/DOM/loadPage.js
@@ -1,7 +1,7 @@
 export function framework() {
     const divFrame = document.createElement('div')
     divFrame.id = "container"
-    divFrame.innerHTML = `<div id="header"><h1>To-do list</h1></div><div id="main"><div><button id="newTask">+</button>add task</div><div></div></div><div id=footer></div>`;
+    divFrame.innerHTML = `<div id="header"><h1>To-do list</h1></div><div id="main"><div id="mainleft"><div><h1>Projects</h1></div><div></div><div><button id="newTask"><h2>+ Add Project</h2></button></div></div><div id="mainright"></div></div><div id=footer></div>`;
     return divFrame
 }
 export function appendNodeToBody(node) {
