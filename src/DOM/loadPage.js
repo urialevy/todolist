76415f98b639d45701f7bf6918748f596951c26f
Uria <dorman010@gmail.com>
2023-10-14 17:34:08 +0100

    basic wireframe for layout is up

--- src/DOM/loadPage.js
+++ src/DOM/loadPage.js
@@ -1,7 +1,25 @@
 export function framework() {
     const divFrame = document.createElement('div')
     divFrame.id = "container"
-    divFrame.innerHTML = `<div id="header"><h1>To-do list</h1></div><div id="main"><div id="mainleft"><div><h1>Projects</h1></div><div></div><div><button id="newTask"><h2>+ Add Project</h2></button></div></div><div id="mainright"></div></div><div id=footer></div>`;
+    divFrame.innerHTML = `<div id="header"><h1>To-do list</h1></div>
+    <div id="main">
+        <div id="mainleft">
+            <div id="projectTitle"><h1>Projects</h1></div>
+            <div id="projectsList">
+                <div class="project">1</div> 
+                <div class="project">2</div> 
+            </div>
+            <div><button id="newTask"><h2>+ Add Project</h2></button></div></div>
+            <div id="mainright">
+                <div id="taskTitle"><h2>Tasks</h2></div>
+                <div id="tasksList">
+                    <div class="task">1</div>
+                    <div class="task">2</div>  
+                    <div class="task">3</div>
+                </div>
+            </div>
+            </div>
+    <div id=footer></div>`;
     return divFrame
 }
 export function appendNodeToBody(node) {
