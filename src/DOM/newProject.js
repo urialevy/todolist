e4670300697e2161d10e99b23dbec8065898648d
Uria <uria@urialevy.com>
2023-12-14 22:24:52 +0000

    style buttons

---
+++ src/DOM/newProject.js
@@ -0,0 +1,9 @@
+export const addProject = () => {
+    let nodeList = document.querySelectorAll('.project')
+    console.log(nodeList)
+}
+
+export const addTask = () => {
+    let nodeList = document.querySelectorAll('.task')
+    console.log(nodeList)
+}
