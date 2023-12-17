638706d861d269276d157bb66113287346970343
Uria <uria@urialevy.com>
2023-12-17 19:19:44 +0000

    remove unnecessary console log

--- src/DOM/newProject.js
+++ src/DOM/newProject.js
@@ -1,9 +1,7 @@
 export const addProject = () => {
     let nodeList = document.querySelectorAll('.project')
-    console.log(nodeList)
 }
 
 export const addTask = () => {
     let nodeList = document.querySelectorAll('.task')
-    console.log(nodeList)
 }
