626b7241ec98c3b170139bb6b770a46bb0e81e75
Uria <uria@urialevy.com>
2023-12-19 20:14:23 +0000

    remove unneeded code

--- src/DOM/newProject.js
+++
@@ -1,7 +0,0 @@
-export const addProject = () => {
-    let nodeList = document.querySelectorAll('.project')
-}
-
-export const addTask = () => {
-    let nodeList = document.querySelectorAll('.task')
-}
